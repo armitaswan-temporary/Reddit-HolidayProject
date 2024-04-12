141142700c88c6e154a16bdb686b320a04d62f02
Armita <its.fake.one.da@gmail.com>
2024-04-12 09:27:08 +0330

    Subreddit complited

---
+++ Reddit-HolidayProject/src/main/java/RedditMain.java
@@ -0,0 +1,18 @@
+import java.util.UUID;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class RedditMain {
+    public static void main(String[] args) {
+        System.out.println("Reddit");
+        System.out.println("1- signin\n2- login");
+        Scanner scanner = new Scanner(System.in);
+
+    }
+
+    public static void runMenu() {
+
+    }
+}
